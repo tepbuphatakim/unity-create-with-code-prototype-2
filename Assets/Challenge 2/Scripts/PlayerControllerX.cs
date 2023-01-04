acdbf813bece5ed7154d42027f59596e520fb79b
tepbuphata.kim <tepbuphata.kim@axivit.com>
2023-01-04 15:40:16 +0700

    Challenge 2 - Play Fetch

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float trapCoolDown = 1;
+    private float timeSinceLastTrap;
+
+    void Start()
+    {
+        timeSinceLastTrap = Time.timeSinceLevelLoad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.timeSinceLevelLoad - timeSinceLastTrap > trapCoolDown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceLastTrap = Time.timeSinceLevelLoad;
+        }
+    }
+}
